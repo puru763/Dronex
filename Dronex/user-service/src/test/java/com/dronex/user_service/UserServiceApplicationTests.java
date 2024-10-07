0a19663b34f293a5a30e94a6cd4f11a1d78355bb
puru763 <purvesh152001@gmail.com>
2024-10-07 13:22:19 +0530

    user service

---
+++ Dronex/user-service/src/test/java/com/dronex/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronex.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
